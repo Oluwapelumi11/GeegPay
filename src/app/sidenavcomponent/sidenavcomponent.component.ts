236fa24e120ac08abb4842ead1576f00e7417804
oluwapelumi11 <oluwapelumiezeliel11@gmail.com>
2024-01-27 17:18:13 +0100

    finally

--- src/app/sidenavcomponent/sidenavcomponent.component.ts
+++ src/app/sidenavcomponent/sidenavcomponent.component.ts
@@ -12,9 +12,9 @@ import { DarkmodetoggleService } from '../darkmodetoggle.service';
                 <!-- top navigation div -->
     <div class="dark:bg-gray-800 w-full h-[488px] flex flex-col gap-[10px] justify-between bg-[#FAFAFA] ">
         <div class="flex sm:justify-center justify-between pr-2 pl-3 py-3 items-center sm:px-0 sm:py-0">
-            <div [routerLink]="['']" [routerLinkActiveOptions]="{exact:true}" routerLinkActive="bg-opacity-70 dark:bg-opacity-100 dark:text-gray-800" class="dark:text-gray-200 flex items-center">
+            <div [routerLink]="['']" [routerLinkActiveOptions]="{exact:true}" routerLinkActive="bg-opacity-70 dark:bg-opacity-100 dark:text-gray-800" class="dark:text-gray-400 flex items-center">
                 <a [routerLink]="['']" [routerLinkActiveOptions]="{exact:true}" routerLinkActive="bg-opacity-70 dark:bg-opacity-100"><img src="../assets/svgs/Vector.svg" alt=""></a>
-                <p class= "text-[#26282C] sm:hidden flex px-3  text-2xl font-semibold font-jakarta first-letter :text-[#34CAA5] first-letter :text-3xl">GeegPay</p>
+                <p class= "text-[#26282C] dark:text-gray-100 sm:hidden flex px-3  text-2xl font-semibold font-jakarta first-letter :text-[#34CAA5] first-letter:text-3xl">GeegPay</p>
             </div>
             
             <button (click)="toggleDarkMode()" class=" w-[46px]  sm:hidden  h-[92px] rounded-[100px] flex flex-col dark:bg-gray-500  bg-[#FFFFFF] p-[8px] gap-[16px] ">
@@ -27,7 +27,7 @@ import { DarkmodetoggleService } from '../darkmodetoggle.service';
         </button>
             
         </div>
-        <div [routerLink]="['']" [routerLinkActiveOptions]="{exact:true}" routerLinkActive="bg-opacity-70 dark:bg-opacity-100 dark:text-gray-800" class="dark:text-gray-200 flex group relative font-bold sm:justify-center  text-right  bg-opacity-0 transition-all ease-in-out duration-700 px-3 sm:px-0 bg-gray-300 py-2" data-tooltip-placement="right" data-tooltip-target="dashboard">
+        <div [routerLink]="['']" [routerLinkActiveOptions]="{exact:true}" routerLinkActive="bg-opacity-70 dark:bg-opacity-100 dark:text-gray-800" class="dark:text-gray-400 flex group relative font-bold sm:justify-center  text-right  bg-opacity-0 transition-all ease-in-out duration-700 px-3 sm:px-0 bg-gray-300 py-2" data-tooltip-placement="right" data-tooltip-target="dashboard">
             <a [routerLink]="['']" [routerLinkActiveOptions]="{exact:true}" routerLinkActive="bg-opacity-70 dark:bg-opacity-100" >
             <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
   <path [attr.fill]="isDarkMode ? '#34CAA5' : '#0D062D'" d="M7.24 2H5.34C3.15 2 2 3.15 2 5.33V7.23C2 9.41 3.15 10.56 5.33 10.56H7.23C9.41 10.56 10.56 9.41 10.56 7.23V5.33C10.57 3.15 9.42 2 7.24 2Z"/>
@@ -37,7 +37,7 @@ import { DarkmodetoggleService } from '../darkmodetoggle.service';
 </svg>
 
 </a>
-            <a  class= "text-[#26282C] px-3 flex sm:hidden dark:text-gray-200 ">Dashboard</a>
+            <a routerLinkActive="bg-opacity-70 dark:bg-opacity-100 dark:text-gray-800" class= " text-[#26282C4] px-3 flex sm:hidden dark:text-gray-200">Dashboard</a>
             
             <span [routerLink]="['']" [routerLinkActiveOptions]="{exact:true}" routerLinkActive="active" class=" yy absolute right-0 hidden group-hover:flex">
                     <img  class="" src="../assets/svgs/Vector active.svg" alt="">
@@ -49,10 +49,10 @@ import { DarkmodetoggleService } from '../darkmodetoggle.service';
                 Dashboard
             <div class="tooltip-arrow" data-popper-arrow></div>
             </div>
-                <div [routerLink]="'/metrics'" routerLinkActive="bg-opacity-70 dark:bg-opacity-100 dark:text-gray-800" class="dark:text-gray-200 flex  group relative font-bold sm:justify-center  text-right hover: bg-opacity-0 transition-all ease-in-out duration-700 px-3 sm:px-0 bg-gray-100 py-2"  data-tooltip-placement="right" data-tooltip-target="sales">
+                <div [routerLink]="'/metrics'" routerLinkActive="bg-opacity-70 dark:bg-opacity-100 dark:text-gray-800" class="dark:text-gray-400 flex  group relative font-bold sm:justify-center  text-right hover: bg-opacity-0 transition-all ease-in-out duration-700 px-3 sm:px-0 bg-gray-100 py-2"  data-tooltip-placement="right" data-tooltip-target="sales">
 
             <a [routerLink]="'/metrics'" routerLinkActive="bg-opacity-70 dark:bg-opacity-100"><img src="../assets/svgs/trend-up.svg" alt=""></a>
-            <a class= "text-[#26282C] px-3 flex sm:hidden dark:text-gray-200 ">Sales Metrics</a>
+            <a routerLinkActive="bg-opacity-70 dark:bg-opacity-100 dark:text-gray-800" class= " text-[#26282C4] px-3 flex sm:hidden dark:text-gray-200">Sales Metrics</a>
             
             <span [routerLink]="['/metrics']" [routerLinkActiveOptions]="{exact:true}" routerLinkActive="active" class=" yy absolute right-0 hidden group-hover:flex">
                 <img class="" src="../assets/svgs/Vector active.svg" alt="">
@@ -63,10 +63,10 @@ import { DarkmodetoggleService } from '../darkmodetoggle.service';
                 Sales Metrics
             <div class="tooltip-arrow" data-popper-arrow></div>
             </div>
-                <div [routerLink]="'profile'" routerLinkActive="bg-opacity-70 dark:bg-opacity-100 dark:text-gray-800" class="dark:text-gray-200 flex group relative font-bold sm:justify-center  text-right hover: bg-opacity-0 transition-all ease-in-out duration-700 px-3 sm:px-0 bg-gray-100 py-2"  data-tooltip-placement="right" data-tooltip-target="profile">
+                <div [routerLink]="'profile'" routerLinkActive="bg-opacity-70 dark:bg-opacity-100 dark:text-gray-800" class="dark:text-gray-400 flex group relative font-bold sm:justify-center  text-right hover: bg-opacity-0 transition-all ease-in-out duration-700 px-3 sm:px-0 bg-gray-100 py-2"  data-tooltip-placement="right" data-tooltip-target="profile">
 
             <a [routerLink]="'profile'" routerLinkActive="bg-opacity-70 dark:bg-opacity-100"><img src="../assets/svgs/profile-2user.svg" alt=""></a>
-            <a class= "text-[#26282C] px-3 flex sm:hidden dark:text-gray-200 ">Profile</a>
+            <a routerLinkActive="bg-opacity-70 dark:bg-opacity-100 dark:text-gray-800" class= " text-[#26282C4] px-3 flex sm:hidden dark:text-gray-200">Profile</a>
 
             <span [routerLink]="['/profile']" [routerLinkActiveOptions]="{exact:true}" routerLinkActive="active" class=" yy absolute right-0 hidden group-hover:flex">
                 <img class="" src="../assets/svgs/Vector active.svg" alt="">
@@ -76,10 +76,10 @@ import { DarkmodetoggleService } from '../darkmodetoggle.service';
                 Profile
             <div class="tooltip-arrow" data-popper-arrow></div>
             </div>
-                <div [routerLink]="'clients'" routerLinkActive="bg-opacity-70 dark:bg-opacity-100 dark:text-gray-800" class="dark:text-gray-200 flex group relative font-bold sm:justify-center  text-right hover: bg-opacity-0 transition-all ease-in-out duration-700 px-3 sm:px-0 bg-gray-100 py-2"  data-tooltip-placement="right" data-tooltip-target="clients">
+                <div [routerLink]="'clients'" routerLinkActive="bg-opacity-70 dark:bg-opacity-100 dark:text-gray-800" class="dark:text-gray-400 flex group relative font-bold sm:justify-center  text-right hover: bg-opacity-0 transition-all ease-in-out duration-700 px-3 sm:px-0 bg-gray-100 py-2"  data-tooltip-placement="right" data-tooltip-target="clients">
 
             <a [routerLink]="'clients'" routerLinkActive="bg-opacity-70 dark:bg-opacity-100"><img src="../assets/svgs/box.svg" alt=""></a>
-            <a [routerLink]="'clients'" routerLinkActive="bg-opacity-70 dark:bg-opacity-100 dark:text-gray-800" class= " text-[#26282C] px-3 flex sm:hidden dark:text-gray-200 ">Clients</a>
+            <a [routerLink]="'clients'" routerLinkActive="bg-opacity-70 dark:bg-opacity-100 dark:text-gray-800" class= " text-[#26282C4] px-3 flex sm:hidden dark:text-gray-200 ">Clients</a>
 
             <span [routerLink]="['/clients']" [routerLinkActiveOptions]="{exact:true}" routerLinkActive="active" class=" yy absolute right-0 hidden group-hover:flex">
                 <img class="" src="../assets/svgs/Vector active.svg" alt="">
@@ -89,10 +89,10 @@ import { DarkmodetoggleService } from '../darkmodetoggle.service';
                 Clients
             <div class="tooltip-arrow" data-popper-arrow></div>
             </div>
-        <div [routerLink]="['/promotions']" routerLinkActive="bg-opacity-70 dark:bg-opacity-100 dark:text-gray-800" class="flex group relative font-bold sm:justify-center  text-right hover: bg-opacity-0 transition-all ease-in-out duration-700 px-3 sm:px-0 bg-gray-100 py-2"  data-tooltip-placement="right" data-tooltip-target="proms">
+        <div [routerLink]="['/promotions']" routerLinkActive="bg-opacity-70 dark:bg-opacity-100 dark:text-gray-800" class="flex group rela4ive font-bold sm:justify-center  text-right hover: bg-opacity-0 transition-all ease-in-out duration-700 px-3 sm:px-0 bg-gray-100 py-2"  data-tooltip-placement="right" data-tooltip-target="proms">
 
             <a [routerLink]="'promotions'" routerLinkActive="bg-opacity-70 dark:bg-opacity-100"><img src="../assets/svgs/discount-shape.svg" alt=""></a>
-            <a  routerLinkActive="bg-opacity-70 dark:bg-opacity-100 dark:text-gray-800" class= " text-[#26282C] px-3 flex sm:hidden dark:text-gray-200 ">Promotions</a>
+            <a  routerLinkActive="bg-opacity-70 dark:bg-opacity-100 dark:text-gray-800" class= " text-[#26282C4] px-3 flex sm:hidden dark:text-gray-200 ">Promotions</a>
             <span [routerLink]="['/promotions']" [routerLinkActiveOptions]="{exact:true}" routerLinkActive="active" class=" yy absolute right-0 hidden group-hover:flex">
                 <img class="" src="../assets/svgs/Vector active.svg" alt="">
 </span>
@@ -101,10 +101,10 @@ import { DarkmodetoggleService } from '../darkmodetoggle.service';
                 Promotions
             <div class="tooltip-arrow" data-popper-arrow></div>
             </div>
-        <div [routerLink]="'informations'" routerLinkActive="bg-opacity-70 dark:bg-opacity-100 dark:text-gray-800" class="dark:text-gray-200 flex group relative font-bold sm:justify-center  text-right hover: bg-opacity-0 transition-all ease-in-out duration-700 px-3 sm:px-0 bg-gray-100 py-2"  data-tooltip-placement="right" data-tooltip-target="info">
+        <div [routerLink]="'informations'" routerLinkActive="bg-opacity-70 dark:bg-opacity-100 dark:text-gray-800" class="dark:text-gray-400 flex group relative font-bold sm:justify-center  text-right hover: bg-opacity-0 transition-all ease-in-out duration-700 px-3 sm:px-0 bg-gray-100 py-2"  data-tooltip-placement="right" data-tooltip-target="info">
 
                     <a [routerLink]="'informations'" routerLinkActive="bg-opacity-70 dark:bg-opacity-100"><img src="../assets/svgs/info-circle.svg" alt=""></a>
-                    <a  class= "text-[#26282C] px-3 flex sm:hidden dark:text-gray-200 ">Informations</a>
+                    <a routerLinkActive="bg-opacity-70 dark:bg-opacity-100 dark:text-gray-800" class= " text-[#26282C4] px-3 flex sm:hidden dark:text-gray-200">Informations</a>
                     
                     <span [routerLink]="['/informations']" [routerLinkActiveOptions]="{exact:true}" routerLinkActive="active" class=" yy absolute right-0 hidden group-hover:flex">
                         <img class="" src="../assets/svgs/Vector active.svg" alt="">
@@ -129,7 +129,7 @@ import { DarkmodetoggleService } from '../darkmodetoggle.service';
     <!--end of top navigation div -->
     <!-- bottom navigation div -->
     <div class=" w-full h-[152px]  flex flex-col gap-[16px] bg-[#FAFAFA] dark:bg-gray-800 items-center justify-end  sm:justify-evenly">
-        <div [routerLink]="'geegpay'" routerLinkActive="bg-opacity-70 dark:bg-opacity-100 dark:text-gray-800" class="dark:text-gray-200 hidden sm:flex"  data-tooltip-placement="right" data-tooltip-target="go">
+        <div [routerLink]="'geegpay'" routerLinkActive="bg-opacity-70 dark:bg-opacity-100 dark:text-gray-800" class="dark:text-gray-400 hidden sm:flex"  data-tooltip-placement="right" data-tooltip-target="go">
            <a [routerLink]="'geegpay'" routerLinkActive="bg-opacity-70 dark:bg-opacity-100">
             <img src="../assets/svgs/arrow-right.svg" alt=""> 
            </a>
@@ -138,7 +138,7 @@ import { DarkmodetoggleService } from '../darkmodetoggle.service';
                 <div class="tooltip-arrow" data-popper-arrow></div>
                 </div>
         </div>
-        <div  [routerLink]="" routerLinkActive="bg-opacity-70 dark:bg-opacity-100 dark:text-gray-800" class="dark:text-gray-200 hidden sm:flex"  data-tooltip-placement="right" data-tooltip-target="settings">
+        <div  [routerLink]="" routerLinkActive="bg-opacity-70 dark:bg-opacity-100 dark:text-gray-800" class="dark:text-gray-400 hidden sm:flex"  data-tooltip-placement="right" data-tooltip-target="settings">
             <a [routerLink]="" routerLinkActive="bg-opacity-70 dark:bg-opacity-100" >
              <img src="../assets/svgs/setting-2.svg" alt=""> 
             </a>
@@ -147,7 +147,7 @@ import { DarkmodetoggleService } from '../darkmodetoggle.service';
                 <div class="tooltip-arrow" data-popper-arrow></div>
                 </div>
          </div>
-         <div  [routerLink]="'logout'" routerLinkActive="bg-opacity-70 dark:bg-opacity-100 dark:text-gray-800" class="dark:text-gray-200 hidden sm:flex"  data-tooltip-placement="right" data-tooltip-target="logout">
+         <div  [routerLink]="'logout'" routerLinkActive="bg-opacity-70 dark:bg-opacity-100 dark:text-gray-800" class="dark:text-gray-400 hidden sm:flex"  data-tooltip-placement="right" data-tooltip-target="logout">
             <a [routerLink]="'logout'" routerLinkActive="bg-opacity-70 dark:bg-opacity-100">
              <img src="../assets/svgs/logout.svg" alt=""> 
             </a>
@@ -159,16 +159,16 @@ import { DarkmodetoggleService } from '../darkmodetoggle.service';
          <div [routerLink]="'settings'" routerLinkActive="bg-opacity-70 dark:bg-opacity-100"  class="flex sm:hidden group  relative font-bold sm:justify-center  text-right hover: bg-opacity-0 w-full transition-all ease-in-out duration-700 px-3 sm:px-0 bg-gray-300 py-2"  >
              
         <a [routerLink]="'settings'" routerLinkActive="bg-opacity-70 dark:bg-opacity-100"><img src="../assets/svgs/setting-2.svg" alt=""></a>
-        <a class= "text-[#26282C] px-3 flex sm:hidden dark:text-gray-200 ">Settings</a>
+        <a routerLinkActive="bg-opacity-70 dark:bg-opacity-100 dark:text-gray-800" class= " text-[#26282C4] px-3 flex sm:hidden dark:text-gray-200">Settings</a>
 
         <span [routerLink]="['/settings']" [routerLinkActiveOptions]="{exact:true}" routerLinkActive="active" class=" yy absolute right-0 hidden group-hover:flex">
             <img class="" src="../assets/svgs/Vector active.svg" alt="">
 </span>
     </div>
-    <div  [routerLink]="'logout'" routerLinkActive="bg-opacity-70 dark:bg-opacity-100 dark:text-gray-800" class="dark:text-gray-200 flex group relative w-full sm:hidden font-bold sm:justify-center  text-right hover: bg-opacity-0 transition-all ease-in-out duration-700 px-3 sm:px-0 bg-gray-300 py-2"  >
+    <div  [routerLink]="'logout'" routerLinkActive="bg-opacity-70 dark:bg-opacity-100 dark:text-gray-800" class="dark:text-gray-400 flex group relative w-full sm:hidden font-bold sm:justify-center  text-right hover: bg-opacity-0 transition-all ease-in-out duration-700 px-3 sm:px-0 bg-gray-300 py-2"  >
         
         <a [routerLink]="'logout'" routerLinkActive="bg-opacity-70 dark:bg-opacity-100"><img src="../assets/svgs/logout.svg" alt=""></a>
-        <a class= "text-[#26282C] px-3 flex sm:hidden dark:text-gray-200 ">Logout</a>
+        <a routerLinkActive="bg-opacity-70 dark:bg-opacity-100 dark:text-gray-800" class= " text-[#26282C4] px-3 flex sm:hidden dark:text-gray-200">Logout</a>
         
         <span [routerLink]="['/logout']" [routerLinkActiveOptions]="{exact:true}" routerLinkActive="active" class=" yy absolute right-0 hidden group-hover:flex">
             <img class="" src="../assets/svgs/Vector active.svg" alt="">
